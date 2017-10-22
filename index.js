569c9958f463095dfbb0793c712bf8042d4851ab
Mahasak Pijittum <mahasak@gmail.com>
2017-10-22 22:30:09 +0700

    update server

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-var http = require('http')
-var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhook', secret: 'myhashsecret' })
+var config = require('./config');
+var http = require('http');
+var createHandler = require('github-webhook-handler');
+var handler = createHandler({
+    path: config.path,
+    secret: config.hash
+});
 
 http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
-  })
-}).listen(7777)
+    console.log("Github-Webhook-Server is listening on port " + config.port);
+    handler(req, res, function (err) {
+        res.statusCode = 404;
+        res.end('no such location');
+    });
+}).listen(config.port);
 
 handler.on('error', function (err) {
-    console.error('Error:', err.message)
-  })
-  
-  handler.on('push', function (event) {
+    console.error('Error:', err.message);
+});
+
+handler.on('push', function (event) {
     console.log('Received a push event for %s to %s',
-      event.payload.repository.name,
-      event.payload.ref)
-  })
-  
-  handler.on('issues', function (event) {
+        event.payload.repository.name,
+        event.payload.ref);
+});
+
+handler.on('issues', function (event) {
     console.log('Received an issue event for %s action=%s: #%d %s',
-      event.payload.repository.name,
-      event.payload.action,
-      event.payload.issue.number,
-      event.payload.issue.title)
-  })
-  
+        event.payload.repository.name,
+        event.payload.action,
+        event.payload.issue.number,
+        event.payload.issue.title);
+});
